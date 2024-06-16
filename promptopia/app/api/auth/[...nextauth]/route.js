f5020d7ddb431e5b94fa5f2480c9991a81c1b04e
Darius <darius.vincent5@gmail.com>
2024-06-16 23:36:54 +0800

    Until Create

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,3 @@
-import nextAuth from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -12,37 +11,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      const userExists = await User.findOne({
-        email: profile.email
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
       });
-
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.username.replace(" ", "").toLowerCase(),
-          image: profile.picture
+  
+      session.user.id = sessionUser._id.toString();
+  
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+  
+        const userExists = await User.findOne({
+          email: profile.email
         });
+  
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.email.split("@")[0],
+            image: profile.picture
+          });
+        }
+  
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
     }
-  }
+  },
 });
 
 export {handler as GET, handler as POST};
--- promptopia/utils/database.js
+++ promptopia/utils/database.js
@@ -12,9 +12,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share-prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "share_prompt",
     })
 
     isConnected = true;
